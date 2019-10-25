a1778c7886db0f357b52112fd0ec4eec6690d65f
Ajit <ajit.sundaresh@gmail.com>
2019-10-25 11:06:23 +0530

    File Added

---
+++ AzureGit/AzureGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First line in visual studio");
+            Console.WriteLine("First change made in visual studio");
+        }
+    }
+}
